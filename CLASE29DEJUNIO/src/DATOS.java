e6c27094c9dbe18de9264df3a96b7405cea925a0
LUNNA <lunna.sosa@uptc.edu.co>
2022-06-29 09:55:32 -0500

    Mi primer commit

---
+++ CLASE29DEJUNIO/src/DATOS.java
@@ -0,0 +1,12 @@
+
+public class DATOS {
+	public static void main(String[] args) {
+		//String s ="Hola";
+		//int x = 3;
+		//String z = (s + x);
+		//System.out.println(z);
+		String s= "A";
+		char c= 'ñ';
+		System.out.println (c+s);
+	}}
+	
